b89a682db8c40cb13e4fa015b442e2d26739021f
naoya80 <tagawa@m.cs.osakafu-u.ac.jp>
2022-05-07 22:56:01 +0900

    3dprint_windoe_projection追加

--- dataset_make/dataset_img_make.py
+++ dataset_make/dataset_img_make.py
@@ -213,7 +213,7 @@ if __name__ == "__main__":
                 char_List2 = Detect_WidthPosition(W_THRESH,w,array_V)
                 #print(char_List2)
                 for j in range(0,len(char_List2)-1, 2):
-                    img_f = syaei_img[int(char_List1[i]):int(char_List1[i+1]), int(char_List2[j]):int(char_List2[j+1])]
+                    img_f = img_mask[int(char_List1[i]):int(char_List1[i+1]), int(char_List2[j]):int(char_List2[j+1])]
                     #cv2.imwrite("result{0}.jpg".format(k),img_f)
                     #k += 1
                     cv2.imwrite(r"C:\Users\Fate2\Desktop\ex3\ex{0}.jpg".format(count), img_f)
