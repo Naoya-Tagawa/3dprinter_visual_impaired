ec165822c87a5b46cc89418f996953cbe5f4bf15
naoyalablaptop <sdb01072@st.osakafu-u.ac.jp>
2022-06-09 22:15:19 +0900

    矢印をどうするかと音声のみmultiにする

--- testimagever1.py
+++ testimagever1.py
@@ -178,6 +178,7 @@ def diff_image_search1(present_frame,before_frame,img_temp,label_temp):
 def diff_image_search(before_frame,present_frame,img_temp,label_temp,before_frame_row1,before_frame_row2,before_frame_row3,before_frame_row4):
     global present_img
     img = cv2.imread("./balck_img.jpg")
+    arrow_img = cv2.imread("./ex6/ex63.jpg")
     img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     print(img.shape)
     cv2.imwrite("black_img.jpg",img)
@@ -192,12 +193,12 @@ def diff_image_search(before_frame,present_frame,img_temp,label_temp,before_fram
     plt.imshow(blue_threshold_present_img)
     plt.show()
     #コーナー検出
-    #try:
-    before_p1,before_p2,before_p3,before_p4 = image_processing.points_extract1(blue_threshold_before_img,before_frame)
-    present_p1,present_p2,present_p3,present_p4 = image_processing.points_extract1(blue_threshold_present_img,present_frame)
-    #except TypeError:
-        #print("Screen cannot be detected")
-        #return before_frame,[] ,[]
+    try:
+        before_p1,before_p2,before_p3,before_p4 = image_processing.points_extract1(blue_threshold_before_img,before_frame)
+        present_p1,present_p2,present_p3,present_p4 = image_processing.points_extract1(blue_threshold_present_img,present_frame)
+    except TypeError:
+        print("Screen cannot be detected")
+        return before_frame,[] ,[]
     #コーナーに従って画像の切り取り
     cut_present = present_frame[present_p1[1]:present_p2[1],present_p2[0]:present_p3[0]]
     cut_before = before_frame[before_p1[1]:before_p2[1],before_p2[0]:before_p3[0]]
@@ -224,13 +225,13 @@ def diff_image_search(before_frame,present_frame,img_temp,label_temp,before_fram
     array_present_H = image_processing.Projection_H(mask_present_img,height_present,width_present)
     presentH_THRESH = max(array_present_H)
     present_char_List = image_processing.Detect_HeightPosition(presentH_THRESH,height_present,array_present_H)
-    print(present_char_List)
     present_char_List = np.reshape(present_char_List,[int(len(present_char_List)/2),2])
     #present_char_List = image_processing.convert_1d_to_2d(present_char_List,2)
     print(present_char_List)
 
     #plt.imshow(syaei_resize_present_img)
     #plt.show()
+    #差分
     frame_diff = cv2.absdiff(blue_threshold_present_img,blue_threshold_before_img)
     #frame_diff = cv2.absdiff(present_frame,before_frame)
     #グレイスケール化
@@ -250,19 +251,15 @@ def diff_image_search(before_frame,present_frame,img_temp,label_temp,before_fram
     array_H = image_processing.Projection_H(mask_cut_diff_frame,height,width)
     H_THRESH = max(array_H)
     char_List1 = image_processing.Detect_HeightPosition(H_THRESH,height,array_H)
-    print(char_List1)
     #char_List1 = image_processing.convert_1d_to_2d(char_List1,2)
     char_List1 = np.reshape(char_List1,[int(len(char_List1)/2),2])
     print(char_List1)
-    knn_model = NearestNeighbors(n_neighbors=1, algorithm='ball_tree').fit(present_char_List) 
-    distances, indices = knn_model.kneighbors(char_List1)
+    #knn_model = NearestNeighbors(n_neighbors=1, algorithm='ball_tree').fit(present_char_List) 
+    #distances, indices = knn_model.kneighbors(char_List1)
     #print(indices)
-    indices = image_processing.get_unique_list(indices)
-    print(indices)
-    #for  i  in indices:
-        #print(i[0])
-        #img_j = cv2.rectangle(syaei_present_img, (0,int(present_char_List[i[0]][0])), (610, int(present_char_List[i[0]][1])), (0,0,255), 2)
-    #cv2.imwrite("diffecence3.jpg",img_j)
+    #indices = image_processing.get_unique_list(indices)
+    #print(indices)
+    
     engine = pyttsx3.init()
     before_frame_row = []
     sabun_count = 0
@@ -297,9 +294,7 @@ def diff_image_search(before_frame,present_frame,img_temp,label_temp,before_fram
             output_text.append(output_text_p)
         
     
-        #plt.imshow(cut_present_img)
         sabun_count = 0
-        #plt.show()
         #engine.runAndWait()
         #cv2,imwrite("yuu.jpg",cut_present_img)
     #if char_List1.size == 0: #差分がなければ
@@ -310,15 +305,15 @@ def diff_image_search(before_frame,present_frame,img_temp,label_temp,before_fram
     #sabun(img,cut_present_img)
     print(output_text)
     engine.say(output_text)
-    if len(indices) == 0:
+    if len(present_char_List) == 0:
         return img,img,img,img
-    elif len(indices) == 1:
+    elif len(present_char_List) == 1:
         return before_frame_row[0] , img,img,img
-    elif len(indices) == 2:
+    elif len(present_char_List) == 2:
         return before_frame_row[0] , before_frame_row[1] ,img,img
-    elif len(indices) == 3:
+    elif len(present_char_List) == 3:
         return before_frame_row[0] , before_frame_row[1] ,before_frame_row[2] ,img
-    elif len(indices) == 4:
+    elif len(present_char_List) == 4:
         return before_frame_row[0] , before_frame_row[1],before_frame_row[2],before_frame_row[3] 
         
 
@@ -349,12 +344,20 @@ def sabun(before_frame_row,present_frame_row):
     array_V = image_processing.Projection_V(frame_diff,height,width)
     W_THRESH = max(array_V)
     char_List2 = image_processing.Detect_WidthPosition(W_THRESH,width,array_V)
-    white_pixcels1 = np.count_nonzero(present_frame_row)
-    white_pixcels2 = np.count_nonzero(frame_diff)
+    white_pixels1 = np.count_nonzero(present_frame_row)
+    white_pixels2 = np.count_nonzero(before_frame_row)
+    sum_white_pixels = white_pixels1 + white_pixels2
+    white_pixels = np.count_nonzero(frame_diff)
+    diff_white_pixels = sum_white_pixels - white_pixels
+    if diff_white_pixels < 0:
+        diff_white_pixels = - diff_white_pixels
+        
+    black_pixels = frame_diff.size - white_pixels
     print("前のフレームとの変化量%")
-    percent = white_pixcels2/white_pixcels1 *100
+    #percent = white_pixels/frame_diff.size *100
+    percent = white_pixels / sum_white_pixels * 100
     print(percent)
-    if percent < 5:
+    if percent < 4:
         return True
     else:
         return False
@@ -398,7 +401,7 @@ def voice(frame,voice_flag):
 if __name__ == "__main__":
     #テンプレートをロード
     img1 = cv2.imread("./camera3/camera14.jpg")
-    img2 = cv2.imread("./camera3/camera15.jpg")
+    img2 = cv2.imread("./camera3/camera16.jpg")
     temp = np.load(r'./dataset2.npz')
     #テンプレート画像を格納
     img_temp = temp['x']
