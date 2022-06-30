19e23cb80730ad32e35fe45ba32e831396b7b306
naoyalablaptop <sdb01072@st.osakafu-u.ac.jp>
2022-06-30 20:07:05 +0900

    音声のみmultiにする

--- Untitled-1.py
+++ Untitled-1.py
@@ -4,10 +4,11 @@ import pyttsx3
 from threading import Thread
 from multiprocessing import process
 import time
-speed = 300
+speed = 200
 #ボリューム
 vol = 1.0
 engine = pyttsx3.init()
+global output_text
 from audio_output import whole_text_read
 def onWord(name, location, length):
     print('word', name, location, length)
@@ -22,75 +23,84 @@ def sy(phrase):
     engine.say(phrase)
     engine.connect('started-word', onWord("kk",flg,10))
     engine.runAndWait()
-    
-def say(phrase,flg):
-    engine = pyttsx3.init()
-    voices = engine.getProperty('voices')
-    engine.setProperty("voice",voices[1].id)
-    #rateはデフォルトが200
-    rate = engine.getProperty('rate')
-    engine.setProperty('rate',speed)
-    #volume デフォルトは1.0 設定は0.0~1.0
-    volume = engine.getProperty('volume')
-    engine.setProperty('volume',vol)
-    count = 0
-    #st = flg.get()
-    for word in phrase:
-        st = flg.value
-        print(st)
-        if word == ' ':
+def threaded(fn):
+    def wrapper(*args, **kwargs):
+        thread = Thread(target=fn, args=args, kwargs=kwargs)
+        thread.start()
+        return thread
+    return wrapper
+@threaded
+def manage_process(p,flg1):
+    while p.is_alive():
+        if flg1.value == 1:
+            print("stop")
+            engine.stop()
+            flg1.value = 0
+        else:
             continue
-        if '/' in word:
-            target = '/'
-            idx = word.find(target)
-            r = word[:idx]
-            engine.say(r)
-            engine.say("スラッシュ")
-            r = word[idx:]
-            engine.say(r)
-            continue
-        if ',' in word:
-            target = ','
-            idx = word.find(target)
-            r = word[:idx]
-            engine.say(r)
-            engine.say("カンマ")
-            r = word[idx:]
-            engine.say(r)
-            continue
-        if "." in word:
-            target = '.'
-            idx = word.find(target)
-            r = word[:idx]
-            engine.say(r)
-            engine.say("ドット")
-            r = word[idx:]
-            engine.say(r)
-            continue
-        engine.say(word)
-        engine.connect('started-word', onWord("kk",st,10))
-    engine.runAndWait()
-    engine.stop()
+
+def say(phrase,flg,output_text):
+    while True:
+        #engine = pyttsx3.init()
+        voices = engine.getProperty('voices')
+        engine.setProperty("voice",voices[1].id)
+        #rateはデフォルトが200
+        rate = engine.getProperty('rate')
+        engine.setProperty('rate',speed)
+        #volume デフォルトは1.0 設定は0.0~1.0
+        volume = engine.getProperty('volume')
+        engine.setProperty('volume',vol)
+        count = 0
+        text = output_text.get()
+        for word in text:
+            #st = flg.value
+            if word == ' ':
+                continue
+            if ',' in word:
+                target = ','
+                idx = word.find(target)
+                r = word[:idx]
+                engine.say(r)
+                engine.say("カンマ")
+                r = word[idx:]
+                engine.say(r)
+                continue
+ 
+            engine.say(word)
+                #engine.say(word)
+        #engine.connect('started-word', onWord("kk",st,10))
+        engine.runAndWait()
+        engine.stop()
         #engine.connect('started-word', onWord("kk",flg,10))
 if __name__ == '__main__':
     engine = pyttsx3.init()
+    q = ['The quick', 'is /very cute']
     flg1 = multiprocessing.Value('i',0)
-    q = ['The','quick','is','very','cute']
+    output_text = multiprocessing.Queue()
+    
+    #output_text = multiprocessing.Array('s',q)
     #whole_text_read(q)
-    er = multiprocessing.Process(target=say,args=(q,flg1))
+    er = multiprocessing.Process(target=say,args=(q,flg1,output_text))
     #flg1 = False
     er.start()
+    t = manage_process(er,flg1)
 #er = Thread(target=say,args=(q,))
 #er = multiprocessing.Process(target=sy,args=('The quick brown fox jumped over the lazy dog.',))
 #er.start()
 #engine.say('The quick brown fox jumped over the lazy dog.')
 #engine.connect('started-word', onWord("kk",20,10))
     print("sleep")
-    time.sleep(1)
+    flg1.value = 0
+    output_text.put(q)
+    output_text.put(q)
+    flg1.value = int(input())
+    time.sleep(5)
+    #output_text.put(q)
     flg1.value = 1
     print(flg1.value)
     print("wake up")
-    flg1.value = 1
+    flg1.value = 0
+    #output_text.put(q)
     #flg2.send(True)
     #flg.put(False)
 #engine.runAndWait()
