d678f219b78c309e7e939ea2e8f7ceff7e14e969
naoyatest <naoyates@gmail.comt>
2022-11-24 15:08:13 +0900

    コード見直せ

--- test_jit.py
+++ test_jit.py
@@ -51,10 +51,14 @@ cv2.waitKey(0)
 
 
 for i,col in enumerate(color):
-    histr = cv2.calcHist([img1],[i],mask_present_img2,[256],[0,256])
-    plt.plot(histr,color = col)
-    plt.xlim([0,256])
-plt.savefig("hist4.png")
+    histr = cv2.calcHist([img],[i],mask_present_img2,[256],[0,256])
+    histr += histr
+plt.plot(histr,color = 'g')
+plt.xlim([0,256])
+
+
+
+plt.savefig("hist6.png")
 plt.show()
 
 
