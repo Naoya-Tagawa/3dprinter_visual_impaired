69b6e7e831c434ad354cab632bd239ad848a173c
naoya <naoya@hack.com>
2023-07-25 19:02:36 +0900

    画像追加

--- dataset_make/dataset_img_make.py
+++ dataset_make/dataset_img_make.py
@@ -8,161 +8,10 @@ import math
 import matplotlib.pyplot as plt
 import glob
 from natsort import natsorted
+from img_deal import sabun1,cut_blue_trans2,mask_make1,make_char_list,get_unique_list,recog_text,projective_transformation2,cut_blue_trans,arrow_exist,mask_make, match_text3,projective_transformation,points_extract1,points_extract2,cut_blue_img1,Projection_H,Projection_V,Detect_HeightPosition,Detect_WidthPosition,match_text,match_text2,sabun,match,cut_blue_img2
 from PIL import Image
-def cut_blue_img(img):
-    c_img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
-    img_hsv = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
-    #ブルーに近いものを切り抜く
-    average_color_per_row = np.average(c_img,axis=0)
-    average_color = np.average(average_color_per_row,axis=0)
-    average_color = np.uint8(average_color)
-    #ブルーの最小値
-    blue_min = np.array([100,130,180],np.uint8)
-    #ブルーの最大値
-    blue_max = np.array([120,255,255],np.uint8)
-    threshold_blue_img = cv2.inRange(img_hsv,blue_min,blue_max)
-    #threshold_blue_img = cv2.cvtColor(threshold_blue_img,cv2.COLOR_GRAY2RGB)
-    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (7,7))
-    close_img = cv2.morphologyEx(threshold_blue_img, cv2.MORPH_CLOSE, kernel, iterations=1)
 
-    #文字の部分を塗りつぶす 
-    cnts = cv2.findContours(close_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    cnts = cnts[0] if len(cnts) == 2 else cnts[1]
-    cv2.fillPoly(close_img, cnts, [255,255,255])
-    #plt.imshow(close_img)
-    #plt.show()
-    return close_img
-
-def points_extract(img):
-    #コーナー検出
-    dst = cv2.goodFeaturesToTrack(img,25,0.01,10)
-    dst = np.int0(dst)
-    #コーナーの中でx座標が最小、最大
-    min_p= np.min(dst,axis=0)
-    max_p = np.max(dst,axis=0)
-    
-    mi_x =[]
-    ma_x = []
-
-    #mi_x.append([min_p[0,0],min_p[0,1]])
-    #ma_x.append([max_p[0,0],max_p[0,1]])
-    for i in dst:
-        x,y = i.ravel()
-    
-        if (x-min_p[0,0]) <=10:
-                mi_x.append([x,y])
-            
-        if (max_p[0,0]-x) <=10:
-                ma_x.append([x,y])
-        #cv2.circle(c_img,(x,y),3,255,1)
-    #cv2.circle(c_img,(591,139),50,255,-1)
-
-    p1 =np.zeros(2)
-    p2 =np.zeros(2)
-    p3 =np.zeros(2)
-    p4 =np.zeros(2)
-
-    p =sorted(mi_x,key = lambda x:x[1])
-    #左上
-    p1 = p[0]
-    #左下
-    p2 = p[-1]
-    p = sorted(ma_x,key = lambda x:x[1])
-    #右上
-    p3 = p[0]
-    #右下
-    p4 = p[-1]
-    return p1,p2,p3,p4
-
-def syaei(img1,p1,p2,p3,p4):
-    #座標
-    #p1 左上
-    #p2 左下
-    #p3 右上
-    #p4 右下
-    #幅
-    w = np.linalg.norm(p3[0]-p1[0])
-    w = math.floor(w)
-    h = np.linalg.norm(p2[1]-p1[1])
-    h = math.floor(h)
-    #pts1はカードの4辺、pts2は変換後の座標
-    pts1 = np.float32([p1,p3,p2,p4])
-    pts2 = np.float32([[0,0], [w,0], [0,h], [w,h]])
-    #射影変換を実施
-    M = cv2.getPerspectiveTransform(pts1, pts2)
-    dst = cv2.warpPerspective(img1, M, (w, h))
-    return dst
-
-def Projection_H(img, height, width):
-    array_H = np.zeros(height)
-    for i in range(height):
-        total_count = 0
-        for j in range(width):
-            temp_pixVal = img[i, j]
-            if (temp_pixVal == 0):
-                total_count += 1
-        array_H[i] = total_count
- 
-    return array_H
- 
- 
-def Projection_V(img, height, width):
-    array_V = np.zeros(width)
-    for i in range(width):
-        total_count = 0
-        for j in range(height):
-            temp_pixVal = img[j, i]
-            if (temp_pixVal == 0):
-                total_count += 1
-        array_V[i] = total_count
- 
- 
-    return array_V
- 
- 
- 
-def Detect_HeightPosition(H_THRESH, height, array_H):
-    char_List = np.array([])
- 
-    flg = False
-    posi1 = 0
-    posi2 = 0
-    for i in range(height):
-        val = array_H[i]
-        if (flg==False and val < H_THRESH):
-            flg = True
-            posi1 = i
- 
-        if (flg == True and val >= H_THRESH):
-            flg = False
-            posi2 = i
-            char_List = np.append(char_List, posi1)
-            char_List = np.append(char_List, posi2)
- 
-    return char_List
-
-
-def Detect_WidthPosition(W_THRESH, width, array_V):
-    char_List = np.array([])
- 
-    flg = False
-    posi1 = 0
-    posi2 = 0
-    for i in range(width):
-        val = array_V[i]
-        if (flg==False and val < W_THRESH):
-            flg = True
-            posi1 = i
- 
-        if (flg == True and val >= W_THRESH):
-            flg = False
-            posi2 = i
-            char_List = np.append(char_List, posi1)
-            char_List = np.append(char_List, posi2)
- 
-    return char_List
- 
- 
+  
 if __name__ == "__main__":
     count = 0
     files = glob.glob('./cha_dataset/*')
@@ -171,32 +20,42 @@ if __name__ == "__main__":
         img = cv2.imread(f)
         #対象画像をロード
         #青い部分のみを二値化
-        close_img = cut_blue_img(img)
+        close_img = cut_blue_img2(img)
+        close_img = mask_make1(close_img)
+        #cv2.imshow("close",close_img)
+        #cv2.waitKey(0)
         #コーナー検出
-        p1,p2,p3,p4 = points_extract(close_img)
+        p1,p2,p3,p4 = points_extract1(close_img)
         #コーナーに従って画像の切り取り
         #img_k = img[p1[1]:p2[1],p2[0]:p3[0]]
         #射影変換
-        syaei_img = syaei(img,p1,p2,p3,p4)
+        #syaei_img = projective_transformation(img,p1,p2,p3,p4)
+        #cv2.imshow("syaei",syaei_img)
+        #cv2.waitKey(0)
         # convert gray scale image
-        kernel = np.ones((3,3),np.uint8)
-        gray_img = cv2.cvtColor(syaei_img, cv2.COLOR_RGB2GRAY)
-        # black white
-        ret, bw_img = cv2.threshold(gray_img, 0, 255, cv2.THRESH_OTSU)
-        #ノイズ除去
-        img_mask = cv2.medianBlur(bw_img,3)
+        #kernel = np.ones((3,3),np.uint8)
+        #gray_img = cv2.cvtColor(syaei_img, cv2.COLOR_RGB2GRAY)
+       # cv2.imshow("gray",gray_img)
+        #cv2.waitKey(0)
+        ## black white
+        #ret, bw_img = cv2.threshold(gray_img, 0, 255, cv2.THRESH_OTSU)
+        #cv2.imshow("bw",bw_img)
+        #cv2.waitKey(0)
+        ##ノイズ除去
+        #img_mask = cv2.medianBlur(bw_img,3)
         #膨張化
-        img_mask = cv2.dilate(bw_img,kernel)
-        height, width = img_mask.shape
- 
+        #img_mask = cv2.dilate(bw_img,kernel)
+        height, width = close_img.shape
+        #cv2.imshow("img_mask",close_img)
+        #cv2.waitKey(0)
         # create projection distribution
-        array_H = Projection_H(img_mask, height, width)
+        array_H = Projection_H(close_img, height, width)
         #array_V = Projection_V(bw_img, height, width)
  
         # detect character height position
         H_THRESH = max(array_H)
         char_List1 = Detect_HeightPosition(H_THRESH, height, array_H)
- 
+        print(char_List1)
         # detect character width position
         #W_THRESH = max(array_V)
         #char_List2 = Detect_WidthPosition(W_THRESH, width, array_V)
@@ -206,16 +65,18 @@ if __name__ == "__main__":
             k=0
             #print("Succeeded in character detection")
             for i in range(0,len(char_List1)-1,2):
-                img_h = img_mask[int(char_List1[i]):int(char_List1[i+1]),:]
+                img_h = close_img[int(char_List1[i]):int(char_List1[i+1]),:]
                 h , w = img_h.shape
                 array_V = Projection_V(img_h,h,w)
                 W_THRESH = max(array_V)
                 char_List2 = Detect_WidthPosition(W_THRESH,w,array_V)
                 #print(char_List2)
                 for j in range(0,len(char_List2)-1, 2):
-                    img_f = img_mask[int(char_List1[i])-1:int(char_List1[i+1])+1, int(char_List2[j])-1:int(char_List2[j+1])+1]
+                    img_f = close_img[int(char_List1[i])-1:int(char_List1[i+1])+1, int(char_List2[j])-1:int(char_List2[j+1])+1]
                     #cv2.imwrite("result{0}.jpg".format(k),img_f)
                     #k += 1
+                    #cv2.imshow("img_f",img_f)
+                    #cv2.waitKey(0)
                     cv2.imwrite("./chara/ex{0}.jpg".format(count), img_f)
                     count += 1
         
